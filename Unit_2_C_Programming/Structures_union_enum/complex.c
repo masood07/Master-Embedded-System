a364aff195a08ad4aae449e923a9c3d9862c2008
Tarek Masood <TAREK MASOOD@Mas3od>
2022-10-08 08:41:24 +0200

    C program to add two complex numbers by passing structure to a function

--- Unit_2_C_Programming/Structures_union_enum/complex.c
+++ Unit_2_C_Programming/Structures_union_enum/complex.c
@@ -11,23 +11,24 @@ struct Scomplex
 	float real;//real member
 	float imaginary;//imaginary member
 };
-struct Scomplex add (struct Scomplex c1,struct Scomplex c2);
+struct Scomplex add (struct Scomplex c1,struct Scomplex c2);//add function prototype
 int main()
 {
 	struct Scomplex c1,c2,sum;
 	printf("For 1st complex number\n");
 	printf("Enter real and imaginary respectively: ");
 	fflush(stdin); fflush(stdout);
-	scanf("%f %f",&c1.real,&c1.imaginary);
+	scanf("%f %f",&c1.real,&c1.imaginary);//insert real and img For 1st complex number
 	printf("For 2st complex number\n");
 	printf("Enter real and imaginary respectively: ");
 	fflush(stdin); fflush(stdout);
-	scanf("%f %f",&c2.real,&c2.imaginary);
-    sum=add(c1,c2);
-	printf("Sum=%g+%gi",sum.real,sum.imaginary);
+	scanf("%f %f",&c2.real,&c2.imaginary);//insert real and img For 2nd complex number
+    sum=add(c1,c2);//call add function
+	printf("Sum=%g+%gi",sum.real,sum.imaginary);//print sum
 
 	return 0;
 }
+//function return sum of two complex number
 struct Scomplex add (struct Scomplex c1,struct Scomplex c2)
 {
 	struct Scomplex sum;
