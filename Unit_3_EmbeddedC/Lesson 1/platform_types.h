d2a48b33ce8c1aef122fb88908337044827c0ced
Tarek masood <110067660+masood07@users.noreply.github.com>
2023-07-03 11:02:44 +0300

    Update AUTOSAR platform_types.h

--- Unit_3_EmbeddedC/Lesson 1/platform_types.h
+++ Unit_3_EmbeddedC/Lesson 1/platform_types.h
@@ -31,12 +31,12 @@ typedef _Bool		boolean;
 typedef int8_t		sint8;
 typedef uint8_t		uint8;
 typedef int16_t		sint16;
-typedef uint16_t		uint16;
+typedef uint16_t	uint16;
 typedef int32_t		sint32;
-typedef uint32_t		uint32;
+typedef uint32_t	uint32;
 typedef int64_t		sint64;
-typedef uint64_t		uint64;
-typedef char		char_t;
+typedef uint64_t	uint64;
+typedef char		  char_t;
 
 typedef volatile int8_t		vint8_t;
 typedef volatile uint8_t	vuint8_t;
