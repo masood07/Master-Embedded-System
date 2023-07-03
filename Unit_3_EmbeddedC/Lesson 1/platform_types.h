f7baecf506700a7e9cabb4cdc834b81f67d2b376
Tarek masood <110067660+masood07@users.noreply.github.com>
2023-07-03 10:45:40 +0300

    updat AUTOSAR platform_types.h

--- Unit_3_EmbeddedC/Lesson 1/platform_types.h
+++ Unit_3_EmbeddedC/Lesson 1/platform_types.h
@@ -11,31 +11,31 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#ifndef _Bool
-#define _Bool unsigned char
+#ifndef	_Bool
+#define	_Bool	unsigned char
 #endif
 
-#define CPU_TYPE 		   CPU_TYPE_32
-#define CPU_BIT_ORDER 	   LSB_FIRST
-#define CPU_BYTE_ORDER    HIGH_BYTE_FIRST
+#define	CPU_TYPE		CPU_TYPE_32
+#define	CPU_BIT_ORDER	LSB_FIRST
+#define	CPU_BYTE_ORDER	HIGH_BYTE_FIRST
 
-#ifndef FALSE
-#define FALSE (boolean)false
+#ifndef	FALSE
+#define	FALSE	(boolean)false
 #endif
 
-#ifndef TRUE
-#define TRUE (boolean)true
+#ifndef 	TRUE
+#define	TRUE	(boolean)true
 #endif
 
 typedef _Bool		boolean;
 typedef int8_t		sint8;
-typedef uint8_t	uint8;
-typedef int16_t	sint16;
-typedef uint16_t	uint16;
-typedef int32_t	sint32;
-typedef uint32_t	uint32;
-typedef int64_t	sint64;
-typedef uint64_t	uint64;
+typedef uint8_t		uint8;
+typedef int16_t		sint16;
+typedef uint16_t		uint16;
+typedef int32_t		sint32;
+typedef uint32_t		uint32;
+typedef int64_t		sint64;
+typedef uint64_t		uint64;
 typedef char		char_t;
 
 typedef volatile int8_t		vint8_t;
